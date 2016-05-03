e5f087149c10f51b63d9cd2a80c693816e7ae6cd
Aaron Dack <aaron@onprem.com>
2016-05-03 08:34:31 -0500

    Small change

--- index.js
+++ index.js
@@ -1,7 +1,1 @@
-'use strict';
-
-var _jquery = require('jquery');
-
-var _jquery2 = _interopRequireDefault(_jquery);
-
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+import $ from 'jquery';
